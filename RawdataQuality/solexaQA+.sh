f826d1719e7bfe062d2123c1b831a68eb090b79e
GigaScience-paper-wang2017 <wanggl@big.ac.cn>
2017-03-23 10:57:59 +0800

    Update solexaQA+.sh

--- RawdataQuality/solexaQA+.sh
+++ RawdataQuality/solexaQA+.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-/disk10/biosoft/SolexaQA+/Linux_x64/SolexaQA++ dynamictrim ${Myraw}_1.fastq ${i}_2.fastq -h 20 -d . 
+/disk10/biosoft/SolexaQA+/Linux_x64/SolexaQA++ analysis ${Myraw}_1.fastq ${Myraw}_2.fastq -d solexaqa/ 
 
-/disk10/biosoft/SolexaQA+/Linux_x64/SolexaQA++ lengthsort ${i}_1.fq.trimmed ${i}_2.fq.trimmed -l 50 -d . 
+/disk10/biosoft/SolexaQA+/Linux_x64/SolexaQA++ dynamictrim ${Myraw}_1.fastq ${Myraw}_2.fastq -h 20 -d solexaqa/ 
 
-/disk10/biosoft/SolexaQA+/Linux_x64/SolexaQA++ analysis S97_03A_CHG003013-YJK15I003-GY_L007_R1.fastq.trim.tidy.fq S97_03A_CHG003013-YJK15I003-GY_L007_R2.fastq.trim.tidy.fq -d solexaqa/ 
+/disk10/biosoft/SolexaQA+/Linux_x64/SolexaQA++ lengthsort ${Myraw}_1.fastq.trimmed ${Myraw}_2.fastq.trimmed -l 50 -d . 
+
