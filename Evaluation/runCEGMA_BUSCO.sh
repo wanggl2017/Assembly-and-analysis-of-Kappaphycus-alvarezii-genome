e60a0b3eed0b85d87f8c43ac925f3cb4f989e834
GigaScience-paper-wang2017 <wanggl@big.ac.cn>
2017-03-24 10:49:28 +0800

    Update runCEGMA_BUSCO.sh

--- Evaluation/runCEGMA_BUSCO.sh
+++ Evaluation/runCEGMA_BUSCO.sh
@@ -10,6 +10,6 @@ export PATH=/path/to/BUSCO/CEGMA_v2.5/bin:$PATH
 
 
 export PATH=/path/to/augustus-3.2.2/bin/:/opt/augustus-3.2.2/scripts/:$PATH
-export AUGUSTUS_CONFIG_PATH="/opt/augustus-3.2.2/config/"
+export AUGUSTUS_CONFIG_PATH="/path/to/augustus-3.2.2/config/"
 
 python BUSCO.py -i genome.fa -o output_kapa -l eukaryota -m geno -c 1
