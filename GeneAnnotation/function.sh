9e008dfac836480d16b502babc91f44806cb503e
GigaScience-paper-wang2017 <wanggl@big.ac.cn>
2017-03-24 11:14:48 +0800

    Update function.sh

--- GeneAnnotation/function.sh
+++ GeneAnnotation/function.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-
+#Blast to databases
+/path/to/blastall -p blastp -d /path/to/database.fasta -i proteins.aa -o /path/to/output.txt -e 1e-5 -F F 
 
 #InterproScan
+java -Xmx2000m -jar server-5.17-56.0-jetty-console.war --headless &
 interproscan.sh -appl Pfam -goterms -pa -iprlookup -t p -i protein.aa -d .
